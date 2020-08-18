8745a4716243d345230542e06de2dc669f470ea1
pitsanu <pitsanu.limpanachaipornkul@thomsonreuters.com>
2020-08-18 16:52:52 +0700

    gg

--- index.js
+++ index.js
@@ -8,31 +8,60 @@ let counterror = 0;
 
 for (let i = 0; i < result.length; i++) {
 
-    if (i > 1501) {
+    if (i > 15000) {
         break;
     } else {
-        console.log(i, result[i])
-        let ws = new Websocket('ws://ec2-18-141-218-122.ap-southeast-1.compute.amazonaws.com/client')
-        ws.on('error', (err) => {
-            ws.terminate();
-            counterror += 1;
-            console.log(counterror, err)
-        })
-
-        ws.on('open', () => {
-            arrs.set(result[i], ws)
-            setTimeout(() => {
-                ws.send(JSON.stringify({ 'action': 'register', 'uuid': result[i].trim() }))
-            }, 3000)
-
-            ws.on('message', (message) => {
-                const result = JSON.parse(message)
-                console.log(i, result);
+        setTimeout(() => {
+            console.log(i, result[i])
+            let ws = new Websocket('ws://ec2-18-141-218-122.ap-southeast-1.compute.amazonaws.com/client')
+            ws.on('error', (err) => {
+                ws.terminate();
+                counterror += 1;
+                console.log(counterror, err)
+                reconnnect(result[i])
             })
 
-            ws.on('close', () => {
-                console.log(result[i].trim(), 'had close')
+            ws.on('open', () => {
+                arrs.set(result[i], ws)
+                setTimeout(() => {
+                    ws.send(JSON.stringify({ 'action': 'register', 'uuid': result[i].trim() }))
+                }, 3000)
+
+                ws.on('message', (message) => {
+                    const result = JSON.parse(message)
+                    console.log(i, result);
+                })
+
+                ws.on('close', () => {
+                    console.log(result[i].trim(), 'had close')
+                })
             })
-        })
+
+        }, i * 1.5);
     }
+}
+
+function reconnnect(uuid) {
+    console.log(uuid)
+    let ws = new Websocket('ws://ec2-18-141-218-122.ap-southeast-1.compute.amazonaws.com/client')
+    ws.on('error', (err) => {
+        ws.terminate();
+        counterror += 1;
+        console.log(counterror, err)
+    })
+
+    ws.on('open', () => {
+        setTimeout(() => {
+            ws.send(JSON.stringify({ 'action': 'register', 'uuid': uuid.trim() }))
+        }, 3000)
+
+        ws.on('message', (message) => {
+            const result = JSON.parse(message)
+            console.log(i, result);
+        })
+
+        ws.on('close', () => {
+            console.log(uuid.trim(), 'had close')
+        })
+    })
 }
